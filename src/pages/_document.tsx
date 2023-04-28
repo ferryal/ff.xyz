be0e0ede232c0368405b6ccb9c9bde06e53f07a9
Ferryal Fajar <ferryal@dagangan.com>
2023-04-28 21:17:06 +0700

    change head doc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,20 +5,20 @@ export default function Document() {
 	return (
 		<Html className="">
 			<Head>
-				<title>Ana Howard - Colourful Haired Human</title>
+				<title>Ferryal Fajar - Software Engineer Frontend @Dagangan</title>
 				<link rel="icon" type="image/png" href="https://anahoward.me/images/eyesemoji.png" />
-				<meta name="title" content="Ana Howard - Colourful Haired Human" />
+				<meta name="title" content="Ferryal Fajar - Software Engineer Frontend @Dagangan" />
 				{/* facebook  / open graph */}
 				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://www.anahoward.me"></meta>
-				<meta property="og:title" content="Ana Howard - Colourful Haired Human" />
+				<meta property="og:url" content="https://www.ferryalfajar.xyz"></meta>
+				<meta property="og:title" content="Ferryal Fajar - Software Engineer Frontend @Dagangan" />
 				<meta property="og:description" content="" />
 				<meta property="og:image" content="https://anahoward.me/images/preview.png"></meta>
 
 				{/* twitter */}
 				<meta property="twitter:card" content="summary_large_image" />
-				<meta property="twitter:url" content="https://www.anahoward.me" />
-				<meta property="twitter:title" content="Ana Howard - Colourful Haired Human" />
+				<meta property="twitter:url" content="https://www.ferryalfajar.xyz" />
+				<meta property="twitter:title" content="Ferryal Fajar - Software Engineer Frontend @Dagangan" />
 				<meta property="twitter:description" content="" />
 				<meta property="twitter:image" content="https://anahoward.me/images/preview.png"></meta>
 
